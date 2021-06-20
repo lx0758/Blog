f14f84bff21dabb6d58a6156018639c760c455fb
6x <lx0758@qq.com>
2021-06-21 00:05:17 +0800

    feat(blog): 接口只使用业务状态码

--- blog/src/main/kotlin/com/liux/blog/config/ExceptionConfig.kt
+++ blog/src/main/kotlin/com/liux/blog/config/ExceptionConfig.kt
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse
 class ExceptionConfig {
 
     companion object {
-        private const val ignoreHttpError = false
+        private const val ignoreHttpError = true
         fun transformHttpStatus(httpStatus: HttpStatus): HttpStatus {
             return if (ignoreHttpError) {
                 HttpStatus.OK
