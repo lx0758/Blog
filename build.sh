8cc9973ef48634b12d90fdf11869c50d33e6fd9e
6x <lx0758@qq.com>
2024-10-11 23:53:24 +0800

    build: 优化编译脚本

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pwd=$PWD
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 error() {
   echo "Build $1 failed!!!"
@@ -9,20 +9,20 @@ error() {
 
 buildAdmin() {
   echo "Building Admin..."
-  cd "$pwd"/admin
+  cd "$SCRIPT_DIR"/admin
   npm install
   if [[ $? != 0 ]]; then error "Admin"; fi
   npm run build
   if [[ $? != 0 ]]; then error "Admin"; fi
-  rm -rf "$pwd"/blog/res/static/admin/*
-  mv "$pwd"/admin/dist/* "$pwd"/blog/res/static/admin/
-  rm -rf "$pwd"/admin/dist/
+  rm -rf "$SCRIPT_DIR"/blog/res/static/admin/*
+  mv "$SCRIPT_DIR"/admin/dist/* "$SCRIPT_DIR"/blog/res/static/admin/
+  rm -rf "$SCRIPT_DIR"/admin/dist/
   echo "Build Admin succeed!"
 }
 
 buildBlog() {
   echo "Building Blog..."
-  cd "$pwd"/blog
+  cd "$SCRIPT_DIR"/blog
   platforms=("linux/amd64" "windows/amd64")
   for platform in "${platforms[@]}"; do
     IFS='/' read -r os arch <<< "$platform"
@@ -33,8 +33,8 @@ buildBlog() {
     GOOS=$os GOARCH=$arch go build -o "${filename}" -v
     if [[ $? != 0 ]]; then error "Blog"; fi
   done
-  rm -rf "$pwd"/blog/res/static/admin/*
-  touch "$pwd"/blog/res/static/admin/.gitkeep
+  rm -rf "$SCRIPT_DIR"/blog/res/static/admin/*
+  touch "$SCRIPT_DIR"/blog/res/static/admin/.gitkeep
   echo "Build Blog succeed!"
 }
 
